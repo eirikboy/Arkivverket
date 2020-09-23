048156beaacc9229ad32fa4ab608cdaed7ba4603
eirikboy <eirik_smile@hotmail.com>
2020-09-23 16:40:40 +0200

    add config for use with api

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -5,6 +5,12 @@ const got = require("got");
 
 const port = process.env.PORT || 3900;
 
+const digitalarkivetApi = got.extend({
+  prefixUrl: "https://media.digitalarkivet.no",
+  headers: {
+    "auth-token": "PLACEHOLDER",
+  },
+});
 app.listen(port, () => {
   console.log("Proxy listening on port " + port);
 });
