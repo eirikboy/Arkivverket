0e2bc6903b8165d3124d8b63d75eb1a20678e50c
eirikboy <eirik_smile@hotmail.com>
2020-09-23 16:35:16 +0200

    removed code and implementetd a component insted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
-import { getSpringRelase } from "./services/springReleaseSerivce";
+import SpringReleaseData from "./SpringReleaseData";
 
 class App extends Component {
-  state = {};
-
-  async componentDidMount() {
-    try {
-      const { data: springRelease } = await getSpringRelase();
-      console.log(springRelease);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   render() {
-    return <h1>TEST</h1>;
+    return (
+      <>
+        <SpringReleaseData />
+      </>
+    );
   }
 }
 
