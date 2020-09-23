4190a650be5645030ab285a92ff18c07616d0585
eirikboy <eirik_smile@hotmail.com>
2020-09-23 16:41:54 +0200

    add getData function

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -11,6 +11,11 @@ const digitalarkivetApi = got.extend({
     "auth-token": "PLACEHOLDER",
   },
 });
+
+async function getData(page) {
+  const urlQuery = { searchParams: { "tags[]": "373", page } };
+  return digitalarkivetApi.get("api/v1/db/browse", urlQuery).json();
+}
 app.listen(port, () => {
   console.log("Proxy listening on port " + port);
 });
