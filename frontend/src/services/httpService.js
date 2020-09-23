37ddd59c9833db6e90a0c0feb25beaf08f62e8ac
eirikboy <eirik_smile@hotmail.com>
2020-09-23 16:37:29 +0200

    removed logic to backend

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-const AUTH_TOKEN = "9189f9c0-e5cd-4fcf-b8fd-ae864c284ec1";
-
-axios.defaults.headers["Authorization"] = AUTH_TOKEN;
-/*axios.defaults.headers.get["Access-Control-Allow-Origin"] =
-  "https://media.digitalarkivet.no";
-axios.defaults.headers.get["Content-Type"] =
-  "application/x-www-form-urlencoded";*/
-
 export default {
   get: axios.get,
 };
