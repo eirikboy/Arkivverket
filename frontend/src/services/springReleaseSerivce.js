f13c966397f5d44e466d6d2860da7126f8006add
eirikboy <eirik_smile@hotmail.com>
2020-09-23 16:38:14 +0200

    cleaned code and seperated logic to backend

--- frontend/src/services/springReleaseSerivce.js
+++ frontend/src/services/springReleaseSerivce.js
@@ -1,19 +1,5 @@
 import http from "./httpService";
-import axios from "axios";
 
-const API = "https://media.digitalarkivet.no/api/v1/db/browse?tags%5B%5D=373";
-
-export function getSpringRelase() {
-  /*return http.get(`${API}`, {
-    mode: "no-cors",
-    headers: { "Access-Control-Allow-Origin": "*" },
-  });*/
-  axios.defaults.baseURL = "https://media.digitalarkivet.no";
-  return axios(API, {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+export function getSpringRelease(page) {
+  return http.get(`http://localhost:3900/data/${page}`);
 }
