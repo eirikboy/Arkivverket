62973a2545ce48cc57179d767b25cf05877b6cb4
eirikboy <eirik_smile@hotmail.com>
2020-09-23 17:26:15 +0200

    add import

---
+++ frontend/src/SpringReleaseData.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { getSpringRelease } from "./services/springReleaseSerivce";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
