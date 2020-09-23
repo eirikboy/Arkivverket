2464cc9df1e3bf9e7a338616b8cda866a333df64
eirikboy <eirik_smile@hotmail.com>
2020-09-23 16:42:45 +0200

    add app.get as a propxy

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -16,6 +16,14 @@ async function getData(page) {
   const urlQuery = { searchParams: { "tags[]": "373", page } };
   return digitalarkivetApi.get("api/v1/db/browse", urlQuery).json();
 }
+
+app.get("/data/:page", cors(), (req, res, next) => {
+  (async () => {
+    const data = await getData(req.params.page);
+    res.send(data);
+  })().catch(next);
+});
+
 app.listen(port, () => {
   console.log("Proxy listening on port " + port);
 });
